6f29ce98f48720495eb2d585d451c6dc4ad06bf3
Li <li.li3@amd.com>
2023-08-17 15:47:38 +0000

    make code more generic

--- ibench_hf.py
+++ ibench_hf.py
@@ -47,6 +47,7 @@ PATH2='/data/llama65b'
 PATH3='/data/falcon40b-instruct'
 PATH4='/data/llama2-70b-chat'
 PATH5='/data/llama2-70b'
+PATH6='/data/llama2-7b-chat'
 
 def main():
 
@@ -205,6 +206,25 @@ def main():
                 sys.exit("Enter valid --platform (MI300X | 2xH100 | 2xMI250)")
         else:
             sys.exit("Enter valid --precision (float16 | bfloat16)")
+    
+    elif args.model == "llama2-7b-chat":
+        PATH = PATH6
+        from transformers import LlamaForCausalLM, LlamaTokenizer
+        tokenizer = LlamaTokenizer.from_pretrained(PATH, padding_side='left')
+        tokenizer.pad_token = tokenizer.eos_token
+        if args.precision == "float16": # pretrained precision : float16
+            model = LlamaForCausalLM.from_pretrained(PATH, torch_dtype=torch.float16, device_map="auto")
+        elif args.precision == "bfloat16":
+            if args.platform == "MI300X":
+                model = LlamaForCausalLM.from_pretrained(PATH, torch_dtype=torch.bfloat16, device_map=DM_MI300X_llamaII70b)
+            elif args.platform == "2xH100":
+                model = LlamaForCausalLM.from_pretrained(PATH, torch_dtype=torch.bfloat16, device_map=DM_2xH100_llamaII70b)
+            elif args.platform == "2xMI250":
+                model = LlamaForCausalLM.from_pretrained(PATH, torch_dtype=torch.bfloat16, device_map=DM_2xMI250_llamaII70b)
+            else:
+                sys.exit("Enter valid --platform (MI300X | 2xH100 | 2xMI250)")
+        else:
+            sys.exit("Enter valid --precision (float16 | bfloat16)")
 
     else:
         sys.exit("Enter valid --model (opt66b | llama65b | falcon40b-instruct | llama2-70b-chat | llama2-70b)")
@@ -339,7 +359,7 @@ def main():
                     else:
                         start_time = perf_counter()
                         if i == 2:
-                            rpd_filename = args.platform + "_llama2.rpd"
+                            rpd_filename = args.platform + "_" + args.model + ".rpd"
                             print("=================rpd_filenanme", rpd_filename)
                             rpdTracerControl.setFilename(name = rpd_filename, append=True)
                             profile = rpdTracerControl()
--- run.sh
+++ run.sh
@@ -1,15 +1,16 @@
 platform=MI300X
+model=llama2-7b-chat
 
-if [ ! -d  "/data/llama2-70b-chat" ]; then
-    mkdir /data/llama2-70b-chat
+if [ ! -d  "/data/${model}" ]; then
+    mkdir /data/${model}
     pip3 --no-cache-dir install --upgrade awscli
-    export AWS_ACCESS_KEY_ID=id_string
-    export AWS_SECRET_ACCESS_KEY=key_string
-    aws --region=us-east-2 s3 cp s3://datasets.dl/llama-2/llama-2-70b-chat-hf/ /data/llama2-70b-chat --recursive
+    export AWS_ACCESS_KEY_ID=
+    export AWS_SECRET_ACCESS_KEY=
+    aws --region=us-east-2 s3 cp s3://datasets.dl/llama-2/llama-2-7b-chat-hf/ /data/${model} --recursive
 fi
 
-rpd_file="${platform}_llama2.rpd"
-json_file="${platform}_llama2.json"
+rpd_file="${platform}_${model}.rpd"
+json_file="${platform}_${model}.json"
 
 echo ${rpd_file}
 echo ${json_file}
@@ -19,7 +20,7 @@ if [ -f "$rpd_file" ] ; then
 fi
 python -m rocpd.schema --create $rpd_file
 
-python ibench_hf.py --model llama2-70b-chat --platform ${platform} --n 1
+python ibench_hf.py --model ${model} --platform ${platform} --n 1
 
 if [ -f "$json_file" ] ; then
     rm "$json_file"
